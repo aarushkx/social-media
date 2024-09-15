0c0b14670b0f44bf796de0fbfda90230204bad32
Aarush Kumar <2005aarushkumar@gmail.com>
2024-09-15 15:14:37 +0530

    Add production endpoints

--- frontend/src/endpoints.js
+++ frontend/src/endpoints.js
@@ -1,5 +1,5 @@
-const AUTH_API_ENDPOINT = "http://localhost:8000/api/v1/auth";
-const USER_API_ENDPOINT = "http://localhost:8000/api/v1/users";
-const POST_API_ENDPOINT = "http://localhost:8000/api/v1/posts";
+const AUTH_API_ENDPOINT = "https://cielit.onrender.com/api/v1/auth";
+const USER_API_ENDPOINT = "https://cielit.onrender.com/api/v1/users";
+const POST_API_ENDPOINT = "https://cielit.onrender.com/api/v1/posts";
 
 export { AUTH_API_ENDPOINT, USER_API_ENDPOINT, POST_API_ENDPOINT };
